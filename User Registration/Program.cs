23f59d43688e4362cbf68e24e62b79cc31488fad
InterstellaR-007 <intrstelr.007@gmail.com>
2020-10-05 23:36:12 +0530

    [Anuj] rule 2

--- User Registration/Program.cs
+++ User Registration/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 
 namespace User_Registration
 {
@@ -6,7 +8,30 @@ namespace User_Registration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String user_Password = "";
+            String pattern = "[a-zA-Z]{8,}";
+            String pattern_UpperCase = "[A-Z]+";
+            Regex regex = new Regex(pattern);
+            Regex regex_UpperCase = new Regex(pattern_UpperCase);
+            Boolean exit_Program = false;
+
+            while (exit_Program != true)
+            {
+
+
+                Console.WriteLine("Enter the password : ");
+                user_Password = Console.ReadLine();
+                Match match = regex.Match(user_Password);
+                Match match_UpperCase = regex_UpperCase.Match(user_Password);
+                if (match.Success && match_UpperCase.Success)
+                {
+                    Console.WriteLine("Valid password :" + user_Password);
+                    exit_Program = true;
+
+                }
+                else
+                    Console.WriteLine("Invalid entered password. Try Again:" + user_Password);
+            }
         }
     }
 }
