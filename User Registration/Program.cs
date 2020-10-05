da27da5fb6c3bcb1960be6823a1cc388b0ee4c74
InterstellaR-007 <intrstelr.007@gmail.com>
2020-10-05 19:44:25 +0530

    [Anuj] uc1 with First Name

--- User Registration/Program.cs
+++ User Registration/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 
 namespace User_Registration
 {
@@ -6,7 +8,27 @@ namespace User_Registration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String first_Name = "";
+            String pattern = "^[A-Z]{1}[a-z]{2,}";
+            Regex regex = new Regex(pattern);
+            Boolean exit_Program = false;
+
+            while (exit_Program != true)
+            {
+
+
+                Console.WriteLine("Enter the first name : ");
+                first_Name = Console.ReadLine();
+                Match match = regex.Match(first_Name);
+                if (match.Success)
+                {
+                    Console.WriteLine("Valid Input :" + first_Name);
+                    exit_Program = true;
+
+                }
+                else
+                    Console.WriteLine("Invalid Input :" + first_Name);
+            }
         }
     }
 }
