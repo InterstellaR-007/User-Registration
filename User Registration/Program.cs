3f35e90278981bdc29f2f430600a9a86f9fe93a2
InterstellaR-007 <intrstelr.007@gmail.com>
2020-10-05 19:54:38 +0530

    [Anuj] Last Name

--- User Registration/Program.cs
+++ User Registration/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 
 namespace User_Registration
 {
@@ -6,7 +8,54 @@ namespace User_Registration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String first_Name = "";
+            String last_Name = "";
+            String[] pattern = { "^[A-Z]{1}[a-z]{2,}", "^[A-Z]{1}[a-z]{2,}" };
+            
+            Boolean exit_Program = false;
+
+            while (exit_Program != true)
+            {
+
+
+                Console.WriteLine("Enter the first name : ");
+                first_Name = Console.ReadLine();
+                Regex regex = new Regex(pattern[0]);
+                Match match = regex.Match(first_Name);
+                if (match.Success)
+                {
+                    Console.WriteLine("Valid Input :" + first_Name);
+                    exit_Program = true;
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input :" + last_Name);
+                    exit_Program = false;
+                }
+            }
+            exit_Program = false;
+
+            while (exit_Program != true)
+            {
+
+                Console.WriteLine("Enter the Last name : ");
+                last_Name = Console.ReadLine();
+                Regex regex = new Regex(pattern[1]);
+                Match match = regex.Match(last_Name);
+                if (match.Success)
+                {
+                    Console.WriteLine("Valid Input :" + last_Name);
+                    exit_Program = true;
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input :" + last_Name);
+                    exit_Program = false;
+                }
+
+            }
+            }
         }
     }
-}
