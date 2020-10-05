e846f01c98ae62ab2a76ca4d8057a22ae7d756e0
InterstellaR-007 <intrstelr.007@gmail.com>
2020-10-05 20:05:19 +0530

    [Anuj] email validate

--- User Registration/Program.cs
+++ User Registration/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 
 namespace User_Registration
 {
@@ -6,7 +8,27 @@ namespace User_Registration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String email_ID = "";
+            String pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
+            Regex regex = new Regex(pattern);
+            Boolean exit_Program = false;
+
+            while (exit_Program != true)
+            {
+
+
+                Console.WriteLine("Enter the email id : ");
+                email_ID = Console.ReadLine();
+                Match match = regex.Match(email_ID);
+                if (match.Success)
+                {
+                    Console.WriteLine("Valid Input :" + email_ID);
+                    exit_Program = true;
+
+                }
+                else
+                    Console.WriteLine("Invalid Input :" + email_ID);
+            }
         }
     }
 }
