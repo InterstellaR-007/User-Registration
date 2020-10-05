349f2fabeed8f3f3fcdffb8353c3e375f3ad7426
InterstellaR-007 <intrstelr.007@gmail.com>
2020-10-05 23:44:56 +0530

    [Anuj] Rule 3

--- User Registration/Program.cs
+++ User Registration/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 
 namespace User_Registration
 {
@@ -6,7 +8,34 @@ namespace User_Registration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String user_Password = "";
+            String pattern = "[a-zA-Z0-9]{8,}";
+            String pattern_UpperCase = "[A-Z]+";
+            String pattern_Numeric = "[0-9]+";
+            Regex regex = new Regex(pattern);
+            Regex regex_UpperCase = new Regex(pattern_UpperCase);
+            Regex regex_Numeric = new Regex(pattern_Numeric);
+            Boolean exit_Program = false;
+
+            while (exit_Program != true)
+            {
+
+
+                Console.WriteLine("Enter the password : ");
+                user_Password = Console.ReadLine();
+                Match match = regex.Match(user_Password);
+                Match match_UpperCase = regex_UpperCase.Match(user_Password);
+                Match match_Numeric = regex_Numeric.Match(user_Password);
+
+                if (match.Success && match_UpperCase.Success && match_Numeric.Success)
+                {
+                    Console.WriteLine("Valid password :" + user_Password);
+                    exit_Program = true;
+
+                }
+                else
+                    Console.WriteLine("Invalid entered password. Try Again:" + match.Success + match_UpperCase.Success + match_Numeric.Success);
+            }
         }
     }
 }
