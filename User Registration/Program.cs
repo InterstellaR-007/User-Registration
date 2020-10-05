196aa5a41fc26f7c348333a3dd45745667ca2721
InterstellaR-007 <intrstelr.007@gmail.com>
2020-10-05 23:09:43 +0530

    [Anuj] Mobile number validation

--- User Registration/Program.cs
+++ User Registration/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 
 namespace User_Registration
 {
@@ -6,7 +8,27 @@ namespace User_Registration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String mob_Number = "";
+            String pattern = "^[0-9]{2}[\\s]{1}[0-9]{10}$";
+            Regex regex = new Regex(pattern);
+            Boolean exit_Program = false;
+
+            while (exit_Program != true)
+            {
+
+
+                Console.WriteLine("Enter the mobile number : ");
+                mob_Number = Console.ReadLine();
+                Match match = regex.Match(mob_Number);
+                if (match.Success)
+                {
+                    Console.WriteLine("Valid Mobile Number :" + mob_Number);
+                    exit_Program = true;
+
+                }
+                else
+                    Console.WriteLine("Invalid Mobile Number :" + mob_Number);
+            }
         }
     }
 }
