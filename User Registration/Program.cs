761c01597dbba46f783e6a007edb14c4fa66c721
InterstellaR-007 <intrstelr.007@gmail.com>
2020-10-05 23:16:56 +0530

    [Anuj] password rule 1 done

--- User Registration/Program.cs
+++ User Registration/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 
 namespace User_Registration
 {
@@ -6,7 +8,27 @@ namespace User_Registration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String user_Password = "";
+            String pattern = "[a-zA-Z]{8,}";
+            Regex regex = new Regex(pattern);
+            Boolean exit_Program = false;
+
+            while (exit_Program != true)
+            {
+
+
+                Console.WriteLine("Enter the password : ");
+                user_Password = Console.ReadLine();
+                Match match = regex.Match(user_Password);
+                if (match.Success)
+                {
+                    Console.WriteLine("Valid password :" + user_Password);
+                    exit_Program = true;
+
+                }
+                else
+                    Console.WriteLine("Invalid entered password. Try Again:" + user_Password);
+            }
         }
     }
 }
